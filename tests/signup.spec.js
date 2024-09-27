f6cfd532b4a236b86308216073cad68b9d508498
RastkoKostic <rastko.kostic@gmail.com>
2024-09-27 15:17:57 +0200

    Added tests for login

--- tests/signup.spec.js
+++ tests/signup.spec.js
@@ -2,22 +2,19 @@ const { test, expect } = require("@playwright/test");
 const exp = require("constants");
 
 test.describe("register", async () => {
-  test.beforeEach("visit app", async ({ page }) => {
-    await page.goto("https://automaticityacademy.ngrok.app/");
-  });
-
   test("Register user", async ({ page }) => {
+    await page.goto("https://automaticityacademy.ngrok.app/");
     await page.getByRole("link", { name: "Register" }).click();
 
     //add user name and assert it's populated with right value
     await page.locator("#username").click();
-    await page.locator("#username").fill("Rale14");
+    await page.locator("#username").fill("Rale66");
     await expect(page.locator("#username")).toHaveValue("Rale2");
 
     //add email and assert it's populated with right value
     await page.locator("#email").click();
-    await page.locator("#email").fill("rale327@gmail.com");
-    await expect(page.locator("#email")).toHaveValue("rale335@gmail.com");
+    await page.locator("#email").fill("rale12@gmail.com");
+    await expect(page.locator("#email")).toHaveValue("rale12@gmail.com");
 
     //add password and assert it's populated with right value
     await page.locator("#password").click();
@@ -25,5 +22,25 @@ test.describe("register", async () => {
     await expect(page.locator("#password")).toHaveValue("Test1234");
 
     await page.getByLabel("Register").click();
+  });
+});
+
+test.describe("login", async () => {
+  test("Login user", async ({ page }) => {
+    await page.goto("https://automaticityacademy.ngrok.app/");
+    await page.getByRole("link", { name: "Log in" }).click();
+
+    //add email and assert it's populated with right value
+    await page.locator("#email").click();
+    await page.locator("#email").fill("rale@gmail.com");
+    await expect(page.locator("#email")).toHaveValue("rale@gmail.com");
+
+    //add password and assert it's populated with right value
+    await page.locator("#password").click();
+    await page.locator("#password").fill("Test1234");
+    await expect(page.locator("#password")).toHaveValue("Test1234");
+
+    await page.getByLabel("Sign In").click();
+    await expect(page.getByText("Buy some stuff bruh")).toBeVisible();
   });
 });
