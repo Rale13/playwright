52cb958a75526d318f40fc9e9502c0c465c1f691
RastkoKostic <rastko.kostic@gmail.com>
2024-10-02 10:31:14 +0200

    refactored register-login spec

---
+++ tests/register-login.spec.js
@@ -0,0 +1,55 @@
+import { test, expect } from "@playwright/test";
+import {
+  generateUserCredentials,
+  HEADINGS,
+  URLS,
+  ERRORS,
+  utils,
+} from "../fixtures";
+import { RegisterPage } from "../pom/modules/ui/registerPage";
+import { LoginPage } from "../pom/modules/ui/loginPage";
+
+let loginPage;
+let registerPage;
+//generate random user credentials
+const { username, email, pass, registerdUser, registerdEmail } =
+  generateUserCredentials(5);
+let loginEmail = email;
+let loginPassword = pass;
+
+test.describe.configure({ mode: "serial" });
+
+test.describe("register", async () => {
+  test.beforeEach("visi page and validte", async ({ page }) => {
+    //visit page and validate
+    await page.goto(URLS["REGISTER"]);
+    //initiate POM class
+    registerPage = new RegisterPage(page);
+  });
+
+  test("register user", async ({ page }) => {
+    //validate page
+    await expect(registerPage.heading).toBeVisible();
+    await expect(registerPage.heading).toHaveText(HEADINGS["REGISTER"]);
+    //fill in form and submit
+    registerPage.register(username, email, pass);
+    //wait for and verify redirect
+    await page.waitForURL(URLS["DASHBOARD"]);
+    await expect(page.getByText(HEADINGS["DASHBOARD"])).toBeVisible();
+  });
+});
+test.describe("login", async () => {
+  test("Login user", async ({ page }) => {
+    //instantiate POM class
+    const loginPage = new LoginPage(page);
+    //visit app and validate
+    await page.goto(URLS["LOGIN"]);
+    await expect(loginPage.heading).toBeVisible();
+    await expect(loginPage.heading).toHaveText(HEADINGS["LOGIN"]);
+    //fill in form and submit
+    loginPage.login(loginEmail, loginPassword);
+    //wait for and verify redirect
+    await page.waitForURL(URLS["DASHBOARD"]);
+    await expect(page.getByText(HEADINGS["DASHBOARD"])).toBeVisible();
+  });
+});
